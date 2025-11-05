ebbfc70443fa1827748abcdda20ded1488072bd4
Yangxin Zhan Du <yangxin.zhan01@estudiant.upf.edu>
2025-11-05 17:45:13 +0100

    changes in peek.sh

--- peek.sh
+++ peek.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/bash
 
 file=$1
-n_lines=$2
+n_lines=${2:-3}
 
-head -n $n_lines $file
-echo .......
-tail -n $n_lines  $file
+
+total_lines=$(wc -l < "$file")
+
+if [[ $total_lines -le $((n_lines * 2)) ]]; then
+  cat "$file"
+else
+  echo "Warning: showing only first and last $n_lines lines."
+  head -n "$n_lines" "$file"
+  echo "..."
+  tail -n "$n_lines" "$file"
+fi
